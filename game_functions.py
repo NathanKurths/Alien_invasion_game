3f0f265e1b13872139d0ead4d7b8a855e91091fb
NathanKurths <nathankurthsm@gmail.com>
2024-07-31 15:26:38 -0300

    att

--- game_functions.py
+++ game_functions.py
@@ -10,7 +10,15 @@ def check_events(ship):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 #Move a espaçonave para a direita
-                ship.rect.centerx += 1
+                ship.moving_right = True
+                #Move a espaçonave para a esquerda
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
             
 def update_screen(ai_settings, screen, ship):
     '''Atualiza as imagens na tela e alterna para a nova tela'''
