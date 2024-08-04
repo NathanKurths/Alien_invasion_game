baff3d293a83da7d9f78d6a9523bd4d4f89c4ceb
NathanKurths <nathankurthsm@gmail.com>
2024-08-04 19:23:01 -0300

    att

--- settings.py
+++ settings.py
@@ -5,4 +5,5 @@ class Settings():
         # Configurações de tela
         self.screen_height = 800
         self.screen_width = 1200
-        self.bg_color=(233,230,230)    
+        self.bg_color=(233,230,230) 
+        self.ship_speed_factor = 1.5  
--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
     
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         '''Inicializa a espaçonave na posição inicial'''
         self.screen = screen
+        self.ai_settings = ai_settings
         # Carrega a imagem da nave e obtém o seu rect
         self.image = pygame.image.load('images/ship.bmp')
         new_size = (50,50)
@@ -16,6 +17,7 @@ class Ship():
         # Inicia cada nova nave na parte inferior da tela em sua posição atual
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
         
         #Flag de movimento
         self.moving_right = False
@@ -24,10 +26,14 @@ class Ship():
     def update(self):
         '''Atualiza a posição da espaçonave de acordo com a flag de movimento'''
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+        
+        #Atualiza o objeto rect de acordo com self.center
+        self.rect.centerx = self.center
+        
     def blitme(self):
         '''Desenha a nave'''
         self.screen.blit(self.image, self.rect)
