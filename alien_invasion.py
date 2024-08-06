e1faa0aba432fd508846e6e61e66b558072c27fe
NathanKurths <nathankurthsm@gmail.com>
2024-08-06 15:47:35 -0300

    att

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,17 +12,20 @@ def run_game():
     
     pygame.display.set_caption(("Alien Invasion"))
     
-    #Cria a espaçonave
+    #Cria uma espaçonave, um grupo de projéteis e um grupo de alienígenas
     ship = Ship(ai_settings, screen)
-    
-    ##Cria um grupo no qual serão armazenados os projéteis
     bullets = Group()
+    aliens = Group()
+    
+    #Cria uma frota de alienígenas
+    gf.create_fleet(ai_settings, screen, ship, aliens)
                                  
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
         
--- settings.py
+++ settings.py
@@ -6,9 +6,16 @@ class Settings():
         self.screen_height = 800
         self.screen_width = 1200
         self.bg_color=(233,230,230) 
-        self.ship_speed_factor = 1.5  
+        self.ship_speed_factor = 1.5 
         
-        #Configuração dos projéteis 
+        # Configuração dos alienígenas 
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        
+        # fleet_direction igual a 1 representa a direita; -1 representa a esquerda
+        self.fleet_direction = 1
+        
+        # Configuração dos projéteis 
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
